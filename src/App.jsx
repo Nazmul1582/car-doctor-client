b64bd5da6962bbc21426a2af0b317f946bcaf31c
Md. Nazmul Hasan <nazmulhasan1582@gmail.com>
2023-10-22 20:53:42 +0600

    initial project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { Outlet } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
+      <h1 >React Car Doctor Website</h1>
     </>
   );
 }
