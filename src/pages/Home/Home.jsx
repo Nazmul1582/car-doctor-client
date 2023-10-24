33df1879b0341a13c4f88c54f53cdcc496221b86
Md. Nazmul Hasan <nazmulhasan1582@gmail.com>
2023-10-24 06:49:16 +0600

    created a banner with carousel and changed header styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Banner from "./Banner/Banner";
+
 const Home = () => {
     return (
-        <div>
-            <h1 className='font-bold text-5xl text-center'>This is Home page</h1>
-        </div>
+        <>
+            <Banner />
+        </>
     );
 };
 
