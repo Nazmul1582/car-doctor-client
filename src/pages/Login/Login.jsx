7fdd6fd045041492159725c2d9fcc049f10374e3
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-30 00:21:19 +0600

    created a sign up page and added this page into route

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,5 @@
 import image from '../../assets/images/login/login.svg'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     const handleLogin = event => {
@@ -13,9 +14,9 @@ const Login = () => {
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
                     <img src={image} alt="login image" />
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 text-center">
                     <form onSubmit={handleLogin} className="card-body">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <h1 className="text-5xl font-bold mb-3">Login now!</h1>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -35,6 +36,7 @@ const Login = () => {
                             <button className="btn btn-error">Login</button>
                         </div>
                     </form>
+                    <p className='mb-10'>New in car doctors? <Link to="/signup" className='text-error font-bold'>Sign Up</Link></p>
                 </div>
             </div>
         </div>
