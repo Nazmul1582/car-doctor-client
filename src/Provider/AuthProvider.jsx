0b00d0fb6a3e4514fc2b3030bdc04214aced2abc
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-30 07:03:56 +0600

    created signup and login functionality

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState } from "react";
-import { getAuth } from 'firebase/auth'
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth'
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
@@ -9,9 +9,32 @@ const AuthProvider = ({children}) => {
     const [users, setUsers] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUsers(currentUser),
+            console.log("current user", currentUser);
+            setLoading(false)
+        })
+        return () =>{
+            return unsubscribe();
+        }
+    }, [])
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
     const authInfo = {
         users,
-        loading
+        loading,
+        createUser,
+        login
     }
 
     return (
