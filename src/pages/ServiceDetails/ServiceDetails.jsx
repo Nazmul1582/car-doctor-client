26c32f920d192c40db5fccb515e885e2ccd2777f
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-31 07:24:24 +0600

    loaded data from server in services and service details page

--- src/pages/ServiceDetails/ServiceDetails.jsx
+++ src/pages/ServiceDetails/ServiceDetails.jsx
@@ -1,6 +1,10 @@
+import { useLoaderData } from 'react-router-dom';
 import image from '../../assets/images/checkout/checkout.png'
 
 const ServiceDetails = () => {
+    const service = useLoaderData()
+    console.log(service);
+    
     return (
         <section>
             <div className="py-20">
@@ -9,6 +13,7 @@ const ServiceDetails = () => {
                     <div className='absolute top-0 left-0 w-full h-full flex items-center bg-gradient-to-r from-[#151515] from-0% to-[rgba(21_21_21/0)] to-100%'>
                         <h2 className='text-white text-5xl font-bold pl-20'>Service Details</h2>
                     </div>
+                    <p className='bg-[#ff3811] text-white absolute bottom-0 left-1/2 transform -translate-x-1/2 py-3 px-8'>Home/Service Details</p>
                 </div>
             </div>
         </section>
